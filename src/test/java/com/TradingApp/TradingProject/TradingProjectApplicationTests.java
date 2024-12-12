750b16794cf41d27f06cd3e1cbc6ebb95fdacce8
DrLaksh <lakshyaacharya.engineer@gmail.com>
2024-12-12 14:21:46 +0530

    First change for Trading App

---
+++ src/test/java/com/TradingApp/TradingProject/TradingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TradingApp.TradingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
